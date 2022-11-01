6772bd534836e7bdcc0c907a1e1275616903a81d
W3zker <W3zker@gmail.com>
2022-11-01 10:50:31 +0200

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $( function() {
     $( "#accordion" ).accordion();
   } );
+
+  $('.owl-carousel').owlCarousel({
+    autoHeight: true,
+    loop: true,
+    margin: 0,
+    nav: true,
+    //navText: [ '', '' ],
+    autoplay: true,
+    autoplayTimeout: 2000,
+    slideBy: 5,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        768: {
+            items: 3,
+        },
+        992: {
+            items: 5,
+        }
+    }
+});
